62352d5ef8e64e34998b59462812c84eccba1a52
Milani Tanya Qurin <89763472+milani14@users.noreply.github.com>
2022-08-30 16:11:33 +0700

    Add files via upload

---
+++ MyProject2/src/myproject2/MyProject2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author PC
+ */
+public class MyProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("MMMMMM                                    MMMMMM        IIIII       LLLL");
+        System.out.println("MMMM MMMM                              MMMM MMMM        IIIII       LLLL");
+        System.out.println("MMMM   MMMM                          MMMM   MMMM        IIIII       LLLL");
+        System.out.println("MMMM     MMMM                      MMMM     MMMM        IIIII       LLLL");
+        System.out.println("MMMM       MMMM                  MMMM       MMMM        IIIII       LLLL");
+        System.out.println("MMMM         MMMM              MMMM         MMMM        IIIII       LLLL");
+        System.out.println("MMMM           MMMM          MMMM           MMMM        IIIII       LLLL");
+        System.out.println("MMMM             MMMM      MMMM             MMMM        IIIII       LLLL");
+        System.out.println("MMMM               MMMM  MMMM               MMMM        IIIII       LLLLLLLLLLLLLLLLLLLL");
+        System.out.println("MMMM                  MMMM                  MMMM        IIIII       LLLLLLLLLLLLLLLL");
+    }
+    
+}
