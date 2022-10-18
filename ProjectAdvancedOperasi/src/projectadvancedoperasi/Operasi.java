131c6c3fdd21d2d6cc8414b5fe791e18c733bcbe
Milani Tanya Qurin <89763472+milani14@users.noreply.github.com>
2022-10-19 06:50:11 +0700

    Add files via upload

---
+++ ProjectAdvancedOperasi/src/projectadvancedoperasi/Operasi.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectadvancedoperasi;
+
+/**
+ *
+ * @author PC
+ */
+public class Operasi {
+    //penjumlahan
+    int jumlahkan(int a, int b){
+        return (a+b);
+    }
+    int jumlahkan(int a, int b, int c){
+        return (a+b+c);
+    }   
+    int perkalian(int a, int b){
+        return (a*b);
+    }    
+    int perkalian(int a, int b, int c){
+        return (a*b*c);
+    }
+    
+    //perkalian
+    double jumlahkan(double a, double b){
+        return (a+b);
+    }    
+    double jumlahkan(double a, double b, double c){
+        return (a+b+c);
+    }  
+    double perkalian(double a, double b){
+        return (a*b);
+    }    
+    double perkalian(double a, double b, double c){
+        return (a*b*c);
+    }
+}
